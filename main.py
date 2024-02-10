41faa771aa7a233bf7b2faeb477ceeeec4d4ca30
Nathaniel Howes <nathan.khowes@gmail.com>
2024-02-10 18:14:36 +0000

    IT WORKS !!!

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import socket
+import pathlib
 
 
 class RequestHandler():
 
     def __init__(self):
-        """
-        - creates a socket object                           - done
-        - recieves a request                                - done
-        - makes a request object                            - 
-        - passes the request object to the correct handler  -
-        """
+        supportedMethods = {'GET':self.handleGet}
+
         s = socket.socket(family=socket.AF_INET)
         s.bind(("127.0.0.1", 80))
         print("Socket ready")
         s.listen()
-        conn, addr = s.accept()
+        self.conn, addr = s.accept()
         print("Connected")
-        request = conn.recv(1000)
+        request = self.conn.recv(1000)
         
-        RequestObject(request)
+        r = RequestObject(request)
+        x = supportedMethods[r.method](r)
 
+    def handleGet(self, request):
+        # just serve up the resource based on relative path
+        header = "HTTP/1.1 200 OK\r\n"
+        header += "Content-Size: "+ str(pathlib.Path('.'+request.resource).stat().st_size)
+
+        body = open("."+request.resource, 'rb').read()
+        file = open("."+request.resource, 'rb')
+        
+        message = header.encode() + "\r\n\r\n".encode() + body
+
+        self.conn.send(message)
 
 class RequestObject():
 
@@ -32,16 +41,20 @@ class RequestObject():
     def parseHeader(self, text):
         lines = text.split("\n")
         print(lines[0])
-        self.method, self.protocol, self.version = lines[0].split(" ")
+        self.method, self.resource, self.protocol = lines[0].split(" ")
         print("method",self.method)
-        print("resource",self.protocol)
-        print("protocol",self.version)
-    
+        print("resource",self.resource)
+        print("protocol",self.protocol)
+
         self.headerDict = {}
         for field in lines[1:-2]:
             print("Parsing field,",field)
             key, value = field.split(": ")
             self.headerDict[key] = value
+
+    def parseBody(self, text):
+        # maybe implement this
+        pass
 
 
 
