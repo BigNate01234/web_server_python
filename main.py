45030426948e7593c376985d07804a3f59105ff7
Nathaniel Howes <nathan.khowes@gmail.com>
2024-02-10 14:41:48 +0000

    added request object with a parseHeader method

--- main.py
+++ main.py
@@ -12,17 +12,35 @@ class RequestHandler():
         """
         s = socket.socket(family=socket.AF_INET)
         s.bind(("127.0.0.1", 80))
-        print("1")
+        print("Socket ready")
         s.listen()
         conn, addr = s.accept()
-        request = conn.recv(1000).decode()
-        print(request)
+        print("Connected")
+        request = conn.recv(1000)
+        
+        RequestObject(request)
 
-        def parseRequest():
-            """
-            just turn this into a dictionary
-            """
-            pass
+
+class RequestObject():
+
+    def __init__(self, content):
+        content = content.decode()
+        header, *body = content.split("\n\n")
+
+        self.parseHeader(header)
+
+    def parseHeader(self, text):
+        lines = text.split("\n")
+        lines[0] = lines[0].replace(" ", "")
+        #print(lines[0])
+        self.method, self.protocol, self.version = lines[0].split("/")
+    
+        headerDict = {}
+        for field in lines[1:-2]:
+            print("Parsing field,",field)
+            key, value = field.split(": ")
+            self.headerDict[key] = value
+
 
 
 if __name__ == "__main__":
